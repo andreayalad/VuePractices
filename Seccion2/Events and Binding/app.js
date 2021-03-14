ef9577d43c6ca73231b9d130187bc386c60d5f4e
andreayalad <alejandrayala01@gmail.com>
2021-03-13 22:59:54 -0600

    Native Event Object

--- Seccion2/Events and Binding/app.js
+++ Seccion2/Events and Binding/app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: " "
     };
   },
   methods :{
-    add() {
-      this.counter = this.counter +1;
+    add(num) {
+      this.counter = this.counter +num;
     },
-    reduce() {
-      this.counter = this.counter -1;
+    reduce(num) {
+      this.counter = this.counter -num;
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' +lastName;
     }
   }
 });
