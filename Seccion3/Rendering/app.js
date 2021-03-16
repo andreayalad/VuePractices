db1916712137b1beafd41fe93ab5720a5c891291
andreayalad <alejandrayala01@gmail.com>
2021-03-15 22:19:09 -0600

    v-if

---
+++ Seccion3/Rendering/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      goals: [], 
+      enteredGoal: ''
+    };
+  },
+  methods:{
+    addGoal(){
+      this.goals.push(this.enteredGoal)
+    }
+  }
+});
+
+app.mount('#user-goals');
