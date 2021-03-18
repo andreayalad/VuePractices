25e93b3ce6a67caeea70a7da1e3315309a268748
andreayalad <alejandrayala01@gmail.com>
2021-03-17 21:55:30 -0600

    Vue Reactivity

---
+++ Seccion5/app.js
@@ -0,0 +1,159 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+//JUST JS
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!'
+};
+
+const handler ={
+  set(target, key, value){
+    if(key === 'message'){
+      target.longMessage = value + 'World!'
+    }
+    target.message = value
+  }
+};
+
+const proxy = new Proxy(data, handler)
+
+proxy.message = 'Hi!'
+
+console.log(proxy.longMessage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
