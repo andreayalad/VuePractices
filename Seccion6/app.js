22972a29b5c4a2ad0b182fd9e637de3c3839e615
andreayalad <alejandrayala01@gmail.com>
2021-03-17 23:27:07 -0600

    Introducing Components

--- Seccion6/app.js
+++ Seccion6/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return{
-            detailsAreVisible: false,
             friends: [
                 { id: 'manuel', 
                 name:'Manuel Lorenz', 
@@ -15,14 +14,43 @@ const app = Vue.createApp({
                 },
             ],
         }
+    },   
+
+   
+});
+app.component('friend-contact', {
+    template: `
+    <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">
+      {{detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{friend.phone}}</li>
+      <li><strong>Email:</strong>{{friend.email}}</li>
+    </ul>
+  </li>
+  ` ,
+    data() {
+        return{
+            friend:
+                { id: 'manuel', 
+                name:'Manuel Lorenz', 
+                phone: '74993640', 
+                email:'mlorenz@gmail.com'
+                },
+            detailsAreVisible: false
+        }
     },
     methods:{
         toggleDetails(){
             this.detailsAreVisible = !this.detailsAreVisible
         }
-    }
+    },
 
-   
 });
 
-app.mount('#app')
+app.mount('#app');
+
+//A COMPONENT IT'S BASICALY LIKE A COSTUME HTML ELEMENT 
+//A VUE COMPONENT IT'S BASICALY LIKE ANOTHER VUE APP BUT AN APP THAT WILL BE CONNECTED TO THE MAIN APP
